1229118f4304862d90405e10cb8aafe61415c5ef
Leandro Arturi <lea.arturi@gmail.com>
2023-08-14 20:36:59 -0300

    If, For, Bufio

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/larturi/golang-curso-udemy/variables"
+	"github.com/larturi/golang-curso-udemy/iteraciones"
 )
 
 func main() {
-	// variables.RestoVariables()
-	estado, texto := variables.ConvertIntToString(12345)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	iteraciones.Iterar()
 }
